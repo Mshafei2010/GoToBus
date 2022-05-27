785ca065b1778b749b92659deeeaef5a9a43e333
Student <student@localhost.localdomain>
2022-05-27 04:05:21 +0200

    ear-web-ejb

---
+++ GoBus-EJB/ejbModule/EJBs/User.java
@@ -0,0 +1,77 @@
+package EJBs;
+
+import java.io.Serializable;
+import java.lang.String;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.*;
+
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@LocalBean
+@Stateless
+@Entity
+@Table(name="USER")
+public class User {
+
+	   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	int id;
+	@Column (name="username")
+	String username;
+	@Column (name="password")
+	String password;
+	@Column (name="full_name")
+	String full_name;
+	@Column (name="role")
+	String role;
+	private static final long serialVersionUID = 1L;
+	
+	public User() {
+		super();
+	}
+ 
+	public int getID() {
+		return this.id;
+	}
+
+	public void setID(int id) {
+		this.id = id;
+	}   
+	public String getusername() {
+		return this.username;
+	}
+	
+	public void setusername(String username) {
+		this.username = username;
+	} 
+	 
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String Password) {
+		this.password = Password;
+	}   
+	public String getFull_name() {
+		return this.full_name;
+	}
+
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	} 
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String Role) {
+		this.role = Role;
+	}
+   
+}
---
+++ GoBus-Web/src/Services/UserServices.java
@@ -0,0 +1,53 @@
+package Services;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.sun.org.apache.bcel.internal.generic.InstructionConstants.Clinit;
+
+import EJBs.User;
+
+@Stateless
+@Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserServices {
+	
+	@PersistenceContext(unitName="GoBusWEB")
+	 private EntityManager entityManager;
+
+	
+	
+	@POST
+	@Path("/register")
+	public String  register(User client) {
+		try {
+		  
+		  entityManager.persist(client);
+		  return "Client Added Successfuly : "+client.getusername();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			return e.getMessage();
+		}
+	}
+	
+	@GET
+	@Path("/getUsers")
+	public List<User> getUsers(){
+		Query query=entityManager.createQuery("SELECT u from User u ");
+		 List<User> persons = query.getResultList();
+		 return persons;
+	}
+
+}
